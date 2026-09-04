c21e0e821c20955c3be7c9ee002d52d468ddf548
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2026-09-04 18:27:46 +0300

    retry sending stats if the sending failed

--- bot/stats_sender.py
+++ bot/stats_sender.py
@@ -1,30 +1,43 @@
 import asyncio
 import logging
-from datetime import datetime
-from typing import Callable, Awaitable, Any
+from datetime import datetime, timedelta
+from typing import Callable, Awaitable, Any, Optional
 
 import config
 from bot.stats import clear_period_stats
+
+RETRY_PERIOD = 300
 
 
 class StatsSender:
     def __init__(self):
         self.last_send_time = datetime.now()
         self.force_reset = False
+        self.next_retry_time: Optional[datetime] = None
 
     def reset(self):
         self.force_reset = True
+        self.next_retry_time = None
+
+    def need_to_send(self) -> bool:
+        if self.next_retry_time is not None and datetime.now() < self.next_retry_time:
+            return False
+        if self.force_reset:
+            return True
+        return (datetime.now() - self.last_send_time).total_seconds() >= config.STATS_PERIOD
 
     async def run(self, send_stats: Callable[[int], Awaitable[Any]]):
         logging.info("Starting StatsSender")
 
         while True:
-            if (datetime.now() - self.last_send_time).total_seconds() >= config.STATS_PERIOD or self.force_reset:
+            if self.need_to_send():
                 try:
                     await send_stats(config.STATS_CHAT_ID)
                     clear_period_stats()
+                    self.last_send_time = datetime.now()
+                    self.force_reset = False
+                    self.next_retry_time = None
                 except Exception:
                     logging.exception("StatsSender failed to send stats")
-                self.last_send_time = datetime.now()
-                self.force_reset = False
+                    self.next_retry_time = datetime.now() + timedelta(seconds=RETRY_PERIOD)
             await asyncio.sleep(10)
